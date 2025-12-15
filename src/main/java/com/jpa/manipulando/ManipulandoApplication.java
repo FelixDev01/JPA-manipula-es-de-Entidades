150b12344eba44843a2805577235b63e39d54305
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2025-12-15 17:17:38 -0300

    First Commit

---
+++ src/main/java/com/jpa/manipulando/ManipulandoApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.manipulando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManipulandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManipulandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa/manipulando/ManipulandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.manipulando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManipulandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
